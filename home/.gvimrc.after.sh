a57a163f6300c5d159a5e794069532fd5a97a56a
Chris Canal <dhtmlgod@gmail.com>
2015-11-13 22:16:28 +0000

    janus try

---
+++ home/.gvimrc.after.sh
@@ -0,0 +1,10 @@
+syntax enable
+set background=light
+colorscheme solarized
+set guifont=Source\ Code\ Pro:h14
+let g:syntastic_javascript_checkers = ['eslint']
+set autoindent
+set noexpandtab
+set tabstop=4
+set shiftwidth=4
+let g:airline#extensions#tabline#enabled = 1
